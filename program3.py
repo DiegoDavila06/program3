75fc570411fb050996f60647b37fd8aaa00f80d9
DiegoDavila06 <diegodav4814@gmail.com>
2023-09-08 13:45:31 -0500

    first commit

---
+++ program3.py
@@ -0,0 +1,36 @@
+def ask_for_input():
+    number_picked = input("Pick a number: ")
+    return number_picked
+
+def echo_user_input(x):
+    print(x)
+
+def add_two_numbers(x,y):
+    z = int(x) + int(y)
+    print(z)
+
+def multiply_three_numbers(x,y,z):
+        a = int(x) * int(y) * int(z)
+        return(a)
+
+def ask_for_numbers(x,y):
+     if int(x) > int(y):
+          print(x)
+     else: print(y)
+     x = any 
+     y = any
+
+    
+
+a = ask_for_input()
+
+echo_user_input(a)
+
+add_two_numbers(ask_for_input(),ask_for_input())
+
+product = multiply_three_numbers(ask_for_input(),ask_for_input(),ask_for_input())
+echo_user_input(product)
+
+ask_for_numbers(ask_for_input(),ask_for_input())
+
+
